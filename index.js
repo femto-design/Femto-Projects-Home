e6f90f41ad1c597f1543b85316c4b8b404247a14
Alexander Craggs <github@femto.host>
2019-03-27 12:11:40 +0000

    Design home

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Hey', user: { name: 'Popey' } })
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
